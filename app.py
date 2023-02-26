019deec17b01ad3ba8f55bc22d97966316493a08
ashwini.sushil <Ashwini.sushil@gmail.com>
2023-02-25 22:51:40 -0700

    first commit to git hub repo

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/usa")
+def home() -> str:
+    print("Inside ghar.")
+    print("Will write more logic later on.")
+    return "Hello from Ashwini inside home Function v.0.0.1!"
+
+@app.route("/usa-store")
+def shop() -> str:
+    print("Inside shop")
+    print("Will write more logic later on.")
+    return "Hello from Ashwini inside shop Function v.0.0.1!"
